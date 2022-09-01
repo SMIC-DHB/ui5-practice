51f46e9f5e573415ebf4d6527a1e4fb5ec4c8529
dhowardb <howard.others6@gmail.com>
2022-09-02 01:47:15 +0800

    commit with ui5 files

---
+++ webapp/index.js
@@ -0,0 +1,11 @@
+// sap.ui.define(["require", "dependency"], function (require, factory) {
+//   "use strict";
+
+//   alert("UI5 is ready to go!");
+// });
+
+sap.ui.define([], function () {
+  "use strict";
+
+  alert("UI5 is ready to go!");
+});
