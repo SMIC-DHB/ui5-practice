9a5ad2394d32b0b1013333dce6b2600f08c1dc68
Dawn Howard L. Baking <dawn.baking@smic.sm.ph>
2022-09-04 12:49:29 +0800

    jsonModel adding inputs with consuming tableData

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,22 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
     "use strict";
     return Controller.extend("sap.ui.ui5-practice.App", {
+      onInit: function () {
+        //set the data view on the view
+        let oData = {
+          recipient: {
+            name: "UI5",
+          },
+        };
+        let oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
       onShowHello: function () {
         //show a native or vanilla Js alert
         MessageToast.show("Hello UI5 via Controller");
