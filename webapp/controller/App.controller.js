d51dfe42e1a4dc969299032a951df66b241ce1c2
dhowardb <howard.others6@gmail.com>
2022-09-05 06:03:18 +0800

    step 8 translatable properties

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  function (Controller, MessageToast, JSONModel) {
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.ui5-practice.App", {
       onInit: function () {
@@ -16,10 +17,25 @@ sap.ui.define(
         };
         let oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+        //set i18n model on view
+        let i18nModel = new ResourceModel({
+          bundleName: "sap.ui.ui5-practice.i18n.i18n",
+          supportedLocales: [""],
+          fallbackLocale: "",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
       onShowHello: function () {
+        //read message from i18n model
+        let oBundle = this.getView().getModel("i18n").getResourceBundle();
+        let sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        //show message
+        MessageToast.show(sMsg);
         //show a native or vanilla Js alert
-        MessageToast.show("Hello UI5 via Controller");
+        // MessageToast.show("Hello UI5 via Controller");
       },
     });
   }
