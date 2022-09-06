ff8e870b475b5ea515605f5063fc2f871f452448
dhowardb <howard.others6@gmail.com>
2022-09-07 06:31:19 +0800

    step 16, adding fragments(created a fragment folder)

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-	["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-	function (Controller, MessageToast) {
+	["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+	function (Controller, MessageToast, Fragment) {
 		"use strict";
 		return Controller.extend("sap.ui.ui5-practice.controller.HelloPanel", {
 			onShowHello: function () {
@@ -14,6 +14,25 @@ sap.ui.define(
 				MessageToast.show(sMsg);
 				//show a native or vanilla Js alert
 				// MessageToast.show("Hello UI5 via Controller");
+			},
+
+			onOpenDialog: function () {
+				let oView = this.getView();
+
+				//create a dialog simply
+				if (!this.byId("helloDialog")) {
+					//load async XML fragment
+					Fragment.load({
+						id: oView.getId(),
+						name: "sap.ui.ui5-practice.fragment.HelloDialog",
+					}).then(function (oDialog) {
+						// connect the dialog to the root view of the component (models, lifecycle)
+						oView.addDependent(oDialog);
+						oDialog.open();
+					});
+				} else {
+					this.byId("helloDialog").open();
+				}
 			},
 		});
 	}
