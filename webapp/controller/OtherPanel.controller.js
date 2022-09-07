5d843c590c7137e91552020d18130fe7854f0e04
dhowardb <howard.others6@gmail.com>
2022-09-07 08:59:14 +0800

    step 17 pt2 adding dialog for other panel and adding states

--- webapp/controller/OtherPanel.controller.js
+++ webapp/controller/OtherPanel.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-	["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-	function (Controller, MessageToast) {
+	["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+	function (Controller, MessageToast, Fragment) {
 		"use strict";
 		return Controller.extend("sap.ui.ui5-practice.controller.OtherPanel", {
 			onShowHello2: function () {
@@ -14,6 +14,50 @@ sap.ui.define(
 				let sMsgTest = oBundleTest.getText("helloMsg", [sRecipientTest]);
 				//show message
 				MessageToast.show(sMsgTest);
+			},
+
+			onOtherOpenDialog: function () {
+				// if (!this.oDefaultMessageDialog) {
+				// 	this.oDefaultMessageDialog = new Dialog({
+				// 		type: DialogType.Message,
+				// 		title: "Default Message",
+				// 		content: new Text({
+				// 			text: "Build enterprise-ready web applications, responsive to all devices and running on the browser of your choice. That's OpenUI5.",
+				// 		}),
+				// 		beginButton: new Button({
+				// 			type: ButtonType.Emphasized,
+				// 			text: "OK",
+				// 			press: function () {
+				// 				this.oDefaultMessageDialog.close();
+				// 			}.bind(this),
+				// 		}),
+				// 	});
+				// 	// }
+				// 	MessageToast.show("Close");
+				// }
+				// MessageToast.show("Open");
+				// this.oDefaultMessageDialog.open();
+				let oView = this.getView();
+
+				//create a dialog simply
+				if (!this.byId("helloOtherDialog")) {
+					//load async XML fragment
+					Fragment.load({
+						id: oView.getId(),
+						name: "sap.ui.ui5-practice.fragment.OtherDialog",
+						controller: this,
+					}).then(function (otherDialog) {
+						// connect the dialog to the root view of the component (models, lifecycle)
+						oView.addDependent(otherDialog);
+						otherDialog.open();
+					});
+				} else {
+					this.byId("helloOtherDialog").open();
+				}
+			},
+
+			onCloseDialog: function () {
+				this.byId("helloOtherDialog").close();
 			},
 		});
 	}
