eef095ee309da5fe7d6ab691fc365b9db7801ba2
dhowardb <howard.others6@gmail.com>
2022-09-10 06:14:44 +0800

    step24 filtering lists via search event

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -3,8 +3,10 @@ sap.ui.define(
 		"sap/ui/core/mvc/Controller",
 		"sap/ui/model/json/JSONModel",
 		"../model/formatter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator",
 	],
-	function (Controller, JSONModel, formatter) {
+	function (Controller, JSONModel, formatter, Filter, FilterOperator) {
 		"use strict";
 
 		return Controller.extend("sap.ui.ui5-practice.controller.InvoiceList", {
@@ -20,6 +22,60 @@ sap.ui.define(
 					}),
 					"view"
 				);
+			},
+
+			onFilterInvoices: function (oEvent) {
+				// build filter array
+				let aFilter = [];
+				let sQuery = oEvent.getParameter("query");
+
+				if (sQuery) {
+					aFilter.push(
+						new Filter("ProductName", FilterOperator.Contains, sQuery)
+					);
+				}
+
+				//filter binding
+				let oList = this.byId("invoiceList");
+				let oBinding = oList.getBinding("items");
+				oBinding.filter(aFilter);
+
+				//let dataInvoices = this.getView().getModel("invoices");
+			},
+
+			onSuggest: function (event) {
+				let aFilters2 = [];
+				var sValue = event.getParameter("suggestValue");
+				if (sValue) {
+					aFilters2 = [
+						new Filter(
+							[
+								new Filter("ProductName", function (sText) {
+									return (
+										(sText || "").toUpperCase().indexOf(sValue.toUpperCase()) >
+										-1
+									);
+								}),
+								// new Filter("Name", function (sDes) {
+								// 	return (
+								// 		(sDes || "").toUpperCase().indexOf(sValue.toUpperCase()) >
+								// 		-1
+								// 	);
+								// }),
+							],
+							false
+						),
+					];
+				}
+
+				console.log(sValue);
+				console.log(aFilters2);
+				let newList = this.byId("invoiceLIst");
+				let newBinding = newList.getBinding("items");
+				newBinding.filter(aFilters2);
+				newList.suggest(aFilters2);
+				// this.oSF.getBinding("invoiceList").filter(aFilters2);
+				// this.oSF.suggest();
 			},
 		});
 	}
--- webapp/controller/OtherLists.controller.js
+++ webapp/controller/OtherLists.controller.js
@@ -3,8 +3,10 @@ sap.ui.define(
 		"sap/ui/core/mvc/Controller",
 		"sap/ui/model/json/JSONModel",
 		"../model/formatter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator",
 	],
-	function (Controller, JSONModel, formatter) {
+	function (Controller, JSONModel, formatter, Filter, FilterOperator) {
 		"use strict";
 
 		return Controller.extend("sap.ui.ui5-practice.controller.OtherLists", {
@@ -26,6 +28,24 @@ sap.ui.define(
 					}),
 					"localDefault"
 				);
+			},
+			onFilterProducts: function (oEvent) {
+				// build filter array
+				let aFilter = [];
+				let sQuery = oEvent.getParameter("query");
+
+				if (sQuery) {
+					aFilter.push(
+						new Filter("ProductName", FilterOperator.Contains, sQuery)
+					);
+				}
+
+				//filter binding
+				let oList = this.byId("productList");
+				let oBinding = oList.getBinding("items");
+				oBinding.filter(aFilter);
+
+				//let dataInvoices = this.getView().getModel("invoices");
 			},
 		});
 	}
